1e1a5385ec9d98df1621a642ea9a5424c1dba036
Scott Walker <scott.walker@valtech.co.uk>
2014-10-01 20:34:11 +0100

    Using play json to represent what we are going to pass on POSTs

--- src/test/scala/Lookup.scala
+++ src/test/scala/Lookup.scala
@@ -1,6 +1,6 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import play.api.libs.json.{JsNumber, JsObject, JsValue, Json}
+import play.api.libs.json._
 
 object Lookup {
 
@@ -37,7 +37,7 @@ object Lookup {
     }
     val asString = Json.stringify(asJson)
     val input = StringBody(asString)
-    val expected = "[7]"
+    val expected = Json.stringify(JsArray(Seq(JsNumber(7))))
     exec(
       http("call legal-neighbours and get expected value").
         post("/mind/legal-neighbours").
@@ -49,9 +49,32 @@ object Lookup {
   }
 
   val `return empty seq when submission is valid but no matches are in scope` = {
-    // Height set to zero.
-    val input = StringBody( """{"scope":{"numVals":1,"numFuncs":2,"numObjects":3,"height":0,"maxExpressionsInFunc":5,"maxFuncsInObject":6,"maxParamsInFunc":7,"maxObjectsInTree":8},"currentNode":1}""")
-    val expected = "[]"
+    val asJson: JsValue = {
+      val scopeAsJson: (String, JsValue) = ("scope", JsObject(
+        fields = Seq(
+          ("numVals", JsNumber(1)),
+          ("numFuncs", JsNumber(2)),
+          ("numObjects", JsNumber(3)),
+          ("height", JsNumber(0)), // Height set to zero.
+          ("maxExpressionsInFunc", JsNumber(5)),
+          ("maxFuncsInObject", JsNumber(6)),
+          ("maxParamsInFunc", JsNumber(7)),
+          ("maxObjectsInTree", JsNumber(8))
+        )
+      ))
+      val currentNodeAsJson: (String, JsValue) = ("currentNode", JsNumber(1))
+
+      JsObject(
+        fields = Seq(
+          scopeAsJson,
+          currentNodeAsJson
+        )
+      )
+    }
+    val asString = Json.stringify(asJson)
+
+    val input = StringBody(asString)
+    val expected = Json.stringify(JsArray(Seq.empty))
     exec(
       http("call legal-neighbours and get expected value").
         post("/mind/legal-neighbours").

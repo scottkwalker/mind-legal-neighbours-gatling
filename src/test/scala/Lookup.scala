3d709b4085612e0d97d38bad9824a85f58297e24
Scott Walker <scott.walker@valtech.co.uk>
2014-10-01 20:27:18 +0100

    Using play json to represent what we are going to pass on POSTs

--- src/test/scala/Lookup.scala
+++ src/test/scala/Lookup.scala
@@ -1,5 +1,6 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import play.api.libs.json.{JsNumber, JsObject, JsValue, Json}
 
 object Lookup {
 
@@ -12,7 +13,30 @@ object Lookup {
     )
 
   val `return ok with seq of ids when submission is valid and legal moves are found` = {
-    val input = StringBody( """{"scope":{"numVals":1,"numFuncs":2,"numObjects":3,"height":4,"maxExpressionsInFunc":5,"maxFuncsInObject":6,"maxParamsInFunc":7,"maxObjectsInTree":8},"currentNode":1}""")
+    val asJson: JsValue = {
+      val scopeAsJson: (String, JsValue) = ("scope", JsObject(
+        fields = Seq(
+          ("numVals", JsNumber(1)),
+          ("numFuncs", JsNumber(2)),
+          ("numObjects", JsNumber(3)),
+          ("height", JsNumber(4)),
+          ("maxExpressionsInFunc", JsNumber(5)),
+          ("maxFuncsInObject", JsNumber(6)),
+          ("maxParamsInFunc", JsNumber(7)),
+          ("maxObjectsInTree", JsNumber(8))
+        )
+      ))
+      val currentNodeAsJson: (String, JsValue) = ("currentNode", JsNumber(1))
+
+      JsObject(
+        fields = Seq(
+          scopeAsJson,
+          currentNodeAsJson
+        )
+      )
+    }
+    val asString = Json.stringify(asJson)
+    val input = StringBody(asString)
     val expected = "[7]"
     exec(
       http("call legal-neighbours and get expected value").
